287225aad7499c7eb2197f9150cd308222defc77
joutace <ojoutace@gmail.com>
2024-10-14 21:49:47 -0300

    feat: add breadcrumbs on index page

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BreadcrumbsComponent } from "../../components/breadcrumbs/breadcrumbs.component";
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+  imports: [BreadcrumbsComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
