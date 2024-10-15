c0a0e9e08f6df625f9224e272b95c0e5990daae9
joutace <ojoutace@gmail.com>
2024-10-15 13:56:36 -0300

    chore: update layout paddings

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { BreadcrumbsComponent } from "../../components/breadcrumbs/breadcrumbs.component";
 import { RelatedProductsComponent } from "../../components/related-products/related-products.component";
 import { ProductDetailsComponent } from "../../components/product-details/product-details.component";
+import { ProductComponent } from "../../components/product/product.component";
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [BreadcrumbsComponent, RelatedProductsComponent, ProductDetailsComponent],
+  imports: [BreadcrumbsComponent, RelatedProductsComponent, ProductDetailsComponent, ProductComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
