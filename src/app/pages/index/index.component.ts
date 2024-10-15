74bb11c4620e1f3dc9bd155121fe3c1f6880f253
joutace <ojoutace@gmail.com>
2024-10-14 23:11:29 -0300

    chore: add new colors and import related products components

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BreadcrumbsComponent } from "../../components/breadcrumbs/breadcrumbs.component";
+import { RelatedProductsComponent } from "../../components/related-products/related-products.component";
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [BreadcrumbsComponent],
+  imports: [BreadcrumbsComponent, RelatedProductsComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
