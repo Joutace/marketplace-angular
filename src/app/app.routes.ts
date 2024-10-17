10ce2140bfc8e8282946774425cc0749cccfb831
joutace <ojoutace@gmail.com>
2024-10-17 17:16:55 -0300

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
 
 export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'index' },
   {
-    path: '',
+    path: 'index',
     component: IndexComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'index',
   },
 ];
