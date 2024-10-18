4141e9d610b88b333029d5cc8fd7c06076248901
joutace <ojoutace@gmail.com>
2024-10-18 18:20:05 -0300

    feat (related-products): cards now display dynamic product list

--- src/app/components/related-products/related-products.component.ts
+++ src/app/components/related-products/related-products.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  CommonModule,
+  NgOptimizedImage,
+  ViewportScroller,
+} from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, map } from 'rxjs';
+import { IProductData } from '../../interfaces/common.interface';
+import { ProductService } from '../../services/product.service';
+import { PricePipe } from '../../pipes/price.pipe';
+import { DiscountPipe } from '../../pipes/discount.pipe';
 
 @Component({
   selector: 'app-related-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgOptimizedImage, PricePipe, DiscountPipe],
   templateUrl: './related-products.component.html',
   styleUrl: './related-products.component.scss',
 })
 export class RelatedProductsComponent {
-  cards = [1, 2, 3, 4];
+  private productService = inject(ProductService);
+  private router = inject(Router);
+  private viewport = inject(ViewportScroller);
+
+  productList$: Observable<IProductData[]> = this.productService
+    .getProducts()
+    .pipe(map((response) => response));
+
+  async redirectPage(product: string) {
+    await this.router.navigate([`index`, product]);
+    this.viewport.scrollToPosition([0, 0]);
+  }
 }
