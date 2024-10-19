572fb419d12e86f36361414dc229ff4c1fa98cfb
joutace <ojoutace@gmail.com>
2024-10-18 22:25:23 -0300

    feat: add cart modal on header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { PricePipe } from '../../pipes/price.pipe';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgOptimizedImage, PricePipe],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  protected cartService = inject(CartService);
 }
