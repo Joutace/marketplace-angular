8eac92ecda53862a79a520e177397cb5a481d1ff
joutace <ojoutace@gmail.com>
2024-10-20 22:42:25 -0300

    feat (breadcrumbs): update breadcrumbs component to display product name as current route

--- src/app/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-breadcrumbs',
   standalone: true,
   imports: [],
   templateUrl: './breadcrumbs.component.html',
-  styleUrl: './breadcrumbs.component.scss'
+  styleUrl: './breadcrumbs.component.scss',
 })
 export class BreadcrumbsComponent {
-
+  public productService = inject(ProductService);
 }
